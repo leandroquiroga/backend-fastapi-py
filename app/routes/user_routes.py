80c7f18b0606f6757b19b87cf10a4a0ba0cfd2ca
leandroquiroga <leandroquiroga9514@gmail.com>
2026-04-11 12:57:29 -0300

    implement PUT and DELETE endpoints

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,20 +1,36 @@
 from fastapi import APIRouter
 from models.user_models import Users
-from utilities.funtions import search_user_by_id, search_user, create_user
-
+from services.user_services import (
+    search_user_by_id,
+    search_user,
+    create_user,
+    update_user,
+    delete_user_id,
+)
 
 router = APIRouter()
 
+
 @router.get("/users")
 async def get_users(skip: int = 0, limit: int = 10) -> list[Users]:
-  return search_user(skip, limit)
+    return search_user(skip, limit)
 
 
 @router.get("/user/{id}")
 async def get_user_by_id(id: int) -> Users | None:
-  return search_user_by_id(id)
+    return search_user_by_id(id)
 
 
 @router.post("/user")
 async def post_create_user(user: Users) -> Users | None:
-  return create_user(user)
+    return create_user(user)
+
+
+@router.put("/user")
+async def put_update_user(user: Users) -> Users | None:
+    return update_user(user)
+
+
+@router.delete("/user/{id}")
+async def delete_user(id: int) -> str:
+    return delete_user_id(id)

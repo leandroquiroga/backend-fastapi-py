0caa3943712d3131294784935808c86502dd1b6d
leandroquiroga <leandroquiroga9514@gmail.com>
2026-04-13 19:58:50 -0300

    add new route

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from routes import user_routes
+from routes import user_router, auth_router
 
 app = FastAPI(
     title="Backend API",
     description="API for the backend of the application",
     version="1.0.0",
 )
-app.include_router(user_routes.router)
+app.include_router(user_router)
+app.include_router(auth_router)

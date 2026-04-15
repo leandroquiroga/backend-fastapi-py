8f31ad16f3745b4e92c841feeb16cb93f8c4f430
leandroquiroga <leandroquiroga9514@gmail.com>
2026-04-15 20:40:22 -0300

    implement POST /login authentication endpoint

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,25 +1,42 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, HTTPException, Depends, status
+from datetime import timedelta
+
 from fastapi.security import OAuth2PasswordRequestForm
 from repositories import users_db
-from models import LoginRequest
-from services import get_current_user, search_user_db
+from utilities import create_access_token, verify_password
+from services import search_user_db
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 auth_router = APIRouter(prefix="/api/v1/auth", tags=["Auth"])
 
+
 @auth_router.post("/login")
-async def login(form: OAuth2PasswordRequestForm = Depends()):
+async def login(form: OAuth2PasswordRequestForm = Depends()) -> dict[str, bytes | str]:
     user_db = next((user for user in users_db if user.username == form.username), None)
     if not user_db:
-        raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST, detail="Incorrect username or disabled user")
-      
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
     user = search_user_db(form.username)
-    
-    if not user or user.password != form.password:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect password")
-      
-    return {"access_token": user.username, "token_type": "bearer"}
-  
-  
-@auth_router.get("/users/me")
-async def read_users_me(user: LoginRequest = Depends(get_current_user)):
-    return user
+
+    if not user or not verify_password(form.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+# @auth_router.get("/users/me")
+# async def read_users_me(user: LoginRequest = Depends(get_current_user)):
+#     return user

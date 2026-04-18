a95527dd6e669922e8f25ab521c388f139677c47
leandroquiroga <leandroquiroga9514@gmail.com>
2026-04-18 11:34:39 -0300

    refine auth services status code for consistency

--- app/services/auth_services.py
+++ app/services/auth_services.py
@@ -2,7 +2,7 @@ import jwt as pyjwt
 from fastapi import HTTPException, status, Depends
 from utilities import oauth2, verify_password
 from models import User
-from repositories import search_user_by_username
+from repositories import search_user_by_username,search_user_by_user_name_response
 from config import SECRET_KEY, ALGORITHM
 
 async def get_current_user(token: str = Depends(oauth2)):
@@ -12,17 +12,17 @@ async def get_current_user(token: str = Depends(oauth2)):
         payload = pyjwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username = payload.get("sub")
         if username is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Username not found in token")
         
     except pyjwt.ExpiredSignatureError: 
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
     except pyjwt.InvalidTokenError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
-    user = search_user_by_username(username)
+    user = search_user_by_user_name_response(username)
     
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
       
     return user
   
@@ -30,8 +30,8 @@ def authenticate_user(username: str, password: str) -> User:
     """Esta función se puede implementar para autenticar al usuario utilizando el token"""
     user = search_user_by_username(username)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     if not verify_password(password, user.password_hash):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Password has not been verified")
     
     return user

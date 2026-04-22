4bf4e6fa437a0a3074ce4027c6fdd705f98ec3be
leequiro <leandroquiroga9514@gmail.com>
2026-04-22 20:08:40 -0300

    remove unnecessary method

--- app/utilities/validation.py
+++
@@ -1,6 +0,0 @@
-from repositories.user_repository import users
-
-def exist_id(id: int) -> bool:
-    # Verifica si el ID existe en la lista de usuarios
-    userAlreadyExists = any(user.id == id for user in users)
-    return userAlreadyExists

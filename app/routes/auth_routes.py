3af62042f42e0bd3dbfe53927b022dc68cdb13f1
leequiro <leandroquiroga9514@gmail.com>
2026-04-28 09:06:17 -0300

    implement data caching for user and auth endpoints

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -3,20 +3,34 @@ from datetime import timedelta
 
 from fastapi.security import OAuth2PasswordRequestForm
 from services import authenticate_user, get_current_user
-from utilities import create_access_token, login_rate_limiter
+from utilities import (
+    create_access_token,
+    login_rate_limiter,
+    cache_get_user,
+    cache_set_user,
+)
 from models import UserResponse
 from config import ACCESS_TOKEN_EXPIRE_MINUTES
 
 auth_router = APIRouter(prefix="/api/v1/auth", tags=["Auth"])
 
+
 @auth_router.post("/login")
-async def login(form: OAuth2PasswordRequestForm = Depends(), _: None = Depends(login_rate_limiter)) -> dict[str, bytes | str]:
+async def login(
+    form: OAuth2PasswordRequestForm = Depends(), _: None = Depends(login_rate_limiter)
+) -> dict[str, bytes | str]:
     user = authenticate_user(form.username, form.password)
+
     access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+    access_token_cached = cache_get_user(f"token_{user.username}")
+    if access_token_cached:
+        return {"access_token": access_token_cached, "token_type": "bearer"}
+    else:
+        access_token = create_access_token(
+            data={"sub": user.username}, expires_delta=access_token_expires
+        )
+        cache_set_user(f"token_{user.username}", access_token)
+        return {"access_token": access_token, "token_type": "bearer"}
 
 
 @auth_router.get("/users/me")
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -3,14 +3,25 @@ from models import UserResponse, UserCreate, UserDB, UserUpdate, ChangePasswordR
 from services import create_user, delete_user_id, update_user
 
 from repositories import search_user, search_user_by_id, change_user_password
-from utilities import require_rol, change_password_rate_limiter, get_user_rate_limiter, get_users_rate_limiter, update_user_rate_limiter
+from utilities import (
+    require_rol,
+    change_password_rate_limiter,
+    get_user_rate_limiter,
+    get_users_rate_limiter,
+    update_user_rate_limiter,
+    cache_get_user,
+    cache_set_user,
+)
 
 user_router = APIRouter(prefix="/api/v1/users", tags=["Users"])
 
 
 @user_router.get("/", response_model=list[UserResponse], status_code=status.HTTP_200_OK)
 async def get_users(
-    skip: int = 0, limit: int = 10, current_user=Depends(require_rol(["admin", "user"])), _: None = Depends(get_users_rate_limiter)
+    skip: int = 0,
+    limit: int = 10,
+    current_user=Depends(require_rol(["admin", "user"])),
+    _: None = Depends(get_users_rate_limiter),
 ):
     """Lista todos los usuarios con paginación"""
     users_db = search_user(skip, limit)
@@ -24,10 +35,18 @@ async def get_users(
 
 @user_router.get("/{id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
 async def get_user_by_id(
-    id: str, current_user=Depends(require_rol(["admin", "user"])), _: None = Depends(get_user_rate_limiter)
+    id: str,
+    current_user=Depends(require_rol(["admin", "user"])),
+    _: None = Depends(get_user_rate_limiter),
 ) -> UserDB:
     """Obtiene un usuario por ID (solo tu perfil o admin puede ver cualquiera)"""
-    user = search_user_by_id(id)
+    cached_user = cache_get_user(id)
+    if cached_user:
+        user = UserDB(**cached_user)
+
+    else:
+        user = search_user_by_id(id)
+        cache_set_user(id, user.model_dump(by_alias=True, mode="json"))
 
     if current_user.role != "admin":
         # Bloquear ver usuarios admin
@@ -40,7 +59,7 @@ async def get_user_by_id(
                 status_code=403, detail="You can only view your own profile"
             )
 
-    return user
+    return UserResponse.model_validate(user.model_dump(by_alias=True, mode="json"))
 
 
 @user_router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
@@ -54,23 +73,27 @@ async def post_create_user(
 
 @user_router.put("/change-password", status_code=status.HTTP_200_OK)
 async def change_password(
-    data: ChangePasswordRequest, current_user=Depends(require_rol(["admin", "user"])), _: None = Depends(change_password_rate_limiter)
+    data: ChangePasswordRequest,
+    current_user=Depends(require_rol(["admin", "user"])),
+    _: None = Depends(change_password_rate_limiter),
 ):
     """Cambia la contraseña del usuario autenticado (siempre tu propia contraseña)"""
     message = change_user_password(str(current_user.id), data)
     return {"message": message}
-      
+
 
 @user_router.put("/{id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
 async def put_update_user(
-    id: str, user_data: UserUpdate, current_user=Depends(require_rol(["admin", "user"])), _: None = Depends(update_user_rate_limiter)
+    id: str,
+    user_data: UserUpdate,
+    current_user=Depends(require_rol(["admin", "user"])),
+    _: None = Depends(update_user_rate_limiter),
 ) -> UserResponse:
     """Actualiza un usuario existente (solo tu perfil o admin puede actualizar cualquiera)"""
 
     if current_user.role != "admin" and str(current_user.id) != id:
         raise HTTPException(
-            status_code=403,
-            detail="You can only update your own profile"
+            status_code=403, detail="You can only update your own profile"
         )
 
     updated_user = update_user(id, user_data)
